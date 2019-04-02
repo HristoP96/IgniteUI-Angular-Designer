f78b761c9aeb5a5ca9c80eda19a5a9f8f24bcc55
HristoP96 <HPopov@infragistics.com>
2019-04-02 18:49:41 +0300

    Setting post request in server

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
   data: Observable<any>;
+  form: Subject<any>;
   constructor(private http: HttpClient) {
+    this.form = new Subject<{}>();
   }
 
   public getData () {
-    return   this.http.get('http://localhost:4205/');
+    return   this.http.get('http://localhost:4205/api/file');
+  }
+
+  public postData(component) {
+    this.http.post<string>('http://localhost:4205/api/form', component).
+    subscribe(answer => {if (answer) {console.log(answer); }});
   }
 }
