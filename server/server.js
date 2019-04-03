03f848f624ac87ee1ad3d5db46f2e505f197b452
HristoP96 <HPopov@infragistics.com>
2019-04-03 19:17:49 +0300

    Adding components templates

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const path = require('path');
 const srcFiles = {}
 const jsonFiles = {};
 var appFiles = {};
+var componentFiles = {};
 const dependencies = require('./dependencies.js');
 
 appFiles["app.component.html"] = fs.readFileSync(path.join(__dirname, "./templates/app.component.html.template"), 'utf8')
@@ -13,6 +14,7 @@ appFiles["app.component.ts"] = fs.readFileSync(path.join(__dirname, "./templates
 appFiles["app.component.scss"] = fs.readFileSync(path.join(__dirname, "./templates/app.component.scss.template"), 'utf8')
 appFiles["app.module.ts"] = fs.readFileSync(path.join(__dirname, "./templates/app.module.ts.template"), 'utf8')
 
+componentTsTemplate = fs.readFileSync(path.join(__dirname, "./component_templates/component.ts.template"), 'utf8')
 
 const app = express()
 const srcDir = '../src/';
@@ -61,14 +63,26 @@ app.route('/api/file').get((req, res) => {
 app.route('/api/form').post((req, res) => {
   const template = req.body;
   console.log(template)
+
   project['template'] = 'angular-cli'
   project['title'] = 'test'
   project['dependencies'] = jsonFiles['package.json']['dependencies']
   project['files']["angular.json"] = JSON.stringify(jsonFiles['angular.json'], null, 2)
+
   var files = {}
   files['src'] = srcFiles;
+
   appFiles = replaceFlags(appFiles, req.body)
   files['src']['app'] = appFiles;
+
+  if(req.body['data']){
+    componentFiles["data.ts"] = `export const ${req.body['data']['name']} = ${JSON.stringify(req.body['data']['value'], null, 2)}`
+  }
+
+  componentFiles[`${req.body['selector']}.component.ts`] = replaceComponentFlags(componentTsTemplate, req.body)
+  componentFiles[`${req.body['selector']}.component.html`] = req.body['template']
+  componentFiles[`${req.body['selector']}.component.scss`] = ''
+  files['src']['app'][`${req.body['selector']}`] = componentFiles ;
   getFilePath('src', files, 'src')
   res.send(project)
 })
@@ -93,4 +107,19 @@ function replaceFlags(fileTree, body){
     fileTree[key] = fileTree[key].replace(/{igComponentName}/g, body['name'])
   })
   return fileTree 
+}
+
+function replaceComponentFlags(file, body){
+  if(body['data']){
+    file = file.replace(/{igDataFile}/g, `import { ${body['data']['name']} } from "./data.ts"`)
+    file = file.replace(/{properties}/g, `public data:any;`)
+    file = file.replace(/{propertiesDefinition}/g, `this.data = ${body['data']['name']};`)
+  } else {
+    file = file.replace(/{igDataFile}/g,'')
+    file = file.replace(/{properties}/g, '')
+    file = file.replace(/{propertiesDefinition}/g,'')
+  }
+    file = file.replace(/{igComponentSelector}/g, body['selector'])
+    file = file.replace(/{igComponentName}/g, body['name'])
+  return file 
 }
