66896c09003d4eadda5b9b7a4697b69f013f0807
HristoP96 <HPopov@infragistics.com>
2019-04-04 14:31:16 +0300

    Refactor server

--- server/server.js
+++ server/server.js
@@ -5,20 +5,9 @@ const fs = require('fs')
 const path = require('path');
 const srcFiles = {}
 const jsonFiles = {};
-var appFiles = {};
-var componentFiles = {};
+const postcss = require('postcss')
+const autoprefixer = require('autoprefixer')
 const dependencies = require('./dependencies.js');
-
-appFiles["app.component.html"] = fs.readFileSync(path.join(__dirname, "./templates/app.component.html.template"), 'utf8')
-appFiles["app.component.ts"] = fs.readFileSync(path.join(__dirname, "./templates/app.component.ts.template"), 'utf8')
-appFiles["app.component.scss"] = fs.readFileSync(path.join(__dirname, "./templates/app.component.scss.template"), 'utf8')
-appFiles["app.module.ts"] = fs.readFileSync(path.join(__dirname, "./templates/app.module.ts.template"), 'utf8')
-
-componentTsTemplate = fs.readFileSync(path.join(__dirname, "./component_templates/component.ts.template"), 'utf8')
-
-const app = express()
-const srcDir = '../src/';
-
 var project = {
   files: {},
   title: {},
@@ -26,76 +15,110 @@ var project = {
   dependencies: {}
 }
 
-fs.readdir(srcDir, (err, files) => {
-  files.forEach(file => {
-    if (file.includes('.ts') || file.includes('.html') || file.includes('.scss')) {
-      srcFiles[file] = fs.readFileSync(path.join(__dirname, `${srcDir + file}`), 'utf8')
-    }
-  })
-})
+var mainTs = fs.readFileSync(path.join(__dirname, "./srcFiles_templates/main.ts.template"), 'utf8')
+var polyfills = fs.readFileSync(path.join(__dirname, "./srcFiles_templates/polyfills.ts.template"), 'utf8')
+var packageJson = JSON.parse(fs.readFileSync(path.join(__dirname, `../package.json`), 'utf8'))
+const app = express()
 
-fs.readdir('../', (err, files) => {
-  files.forEach(file => {
-    if (file.includes('package.json') || file.includes('angular.json')) {
-      jsonFiles[file] = JSON.parse(fs.readFileSync(path.join(__dirname, `../${file}`), 'utf8'))
-    }
-  })
-})
+// fs.readdir(srcDir, (err, files) => {
+//   files.forEach(file => {
+//     if (file.includes('polyfills') || file.includes('.html') || file.includes('.scss')) {
+//       srcFiles[file] = fs.readFileSync(path.join(__dirname, `${srcDir + file}`), 'utf8')
+//     }
+//   })
+// })
+
+// fs.readdir('../', (err, files) => {
+//   files.forEach(file => {
+//     if (file.includes('package.json') ) {
+//       jsonFiles["package.json"] = JSON.parse(fs.readFileSync(path.join(__dirname, `../${file}`), 'utf8'))
+//     }else if( file.includes('angular.json') ){
+//       jsonFiles["angular.json"] = JSON.parse(fs.readFileSync(path.join(__dirname, `../${file}`), 'utf8'))
+//     }
+//   })
+// })
 
 var corsOptions = {
   origin: 'http://localhost:4200',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
 }
+
 app.use(cors(corsOptions))
 app.use(bodyParser.urlencoded({
   extended: true
 }))
 app.use(bodyParser.json())
-app.listen(4205, () => {
-  console.log('Server Started')
-})
 
-
-app.route('/api/file').get((req, res) => {
- // res.send(files);
-});
 
 app.route('/api/form').post((req, res) => {
-  const template = req.body;
-  console.log(template)
+  var srcFiles = {}
+  var appFiles = {};
+  var componentFiles = {};
+  var allFilesTree = {}
+  var angularJson ={}
+  //Generate source files
+  srcFiles["index.html"] = fs.readFileSync(path.join(__dirname, "./srcFiles_templates/index.html.template"), 'utf8')
+  srcFiles["main.ts"] = mainTs
+  srcFiles["polyfills.ts"] = polyfills
 
+  //Generate appcomponent files
+  appFiles["app.component.html"] = fs.readFileSync(path.join(__dirname, "./app_templates/app.component.html.template"), 'utf8')
+  appFiles["app.component.ts"] = fs.readFileSync(path.join(__dirname, "./app_templates/app.component.ts.template"), 'utf8')
+  appFiles["app.component.scss"] = fs.readFileSync(path.join(__dirname, "./app_templates/app.component.scss.template"), 'utf8')
+  appFiles["app.module.ts"] = fs.readFileSync(path.join(__dirname, "./app_templates/app.module.ts.template"), 'utf8')
+
+  //Initialize stackBlitz project core files
   project['template'] = 'angular-cli'
-  project['title'] = 'test'
-  project['dependencies'] = jsonFiles['package.json']['dependencies']
-  project['files']["angular.json"] = JSON.stringify(jsonFiles['angular.json'], null, 2)
+  project['title'] = 'Ignite UI for Angular'
+  project['dependencies'] = packageJson['dependencies']
 
-  var files = {}
-  files['src'] = srcFiles;
+  if(req.body['style'] === 0){
+    srcFiles["styles.scss"] = fs.readFileSync(path.join(__dirname, "./srcFiles_templates/styles.scss.template"), 'utf8')
+      angularJson = JSON.parse(fs.readFileSync(path.join(__dirname, "./srcFiles_templates/angular.json.template"), 'utf8')) 
+    project['files']["angular.json"] = JSON.stringify(angularJson, null, 2)
+  }else{
+    // srcFiles["styles.css"] = postcss([autoprefixer({browsers: ["last 5 versions", "> 3%"], grid: true})])
+    //                         .process(fs.readFileSync(path.join(__dirname, "../srcFiles_templates/styles.scss.template"), 'utf8')).css.toString()
+    // project['files']["angular.json"] = JSON.stringify(fs.readFileSync(path.join(__dirname, "./css_support_templates/angular-css-support.json.template")), null, 2)
+  }
+  
+  // Add source files to the files tree
+  allFilesTree['src'] = srcFiles;
 
   appFiles = replaceFlags(appFiles, req.body)
-  files['src']['app'] = appFiles;
 
+  //Add app files to the source tree
+  allFilesTree['src']['app'] = appFiles;
+
+  componentTsTemplate = fs.readFileSync(path.join(__dirname, "./component_templates/component.ts.template"), 'utf8')
+
+  // Add the data in the date file 
   if(req.body['data']){
     componentFiles["data.ts"] = `export const ${req.body['data']['name']} = ${JSON.stringify(req.body['data']['value'], null, 2)}`
   }
 
+  //Initialize sample component
   componentFiles[`${req.body['selector']}.component.ts`] = replaceComponentFlags(componentTsTemplate, req.body)
   componentFiles[`${req.body['selector']}.component.html`] = req.body['template']
   componentFiles[`${req.body['selector']}.component.scss`] = ''
-  files['src']['app'][`${req.body['selector']}`] = componentFiles ;
-  getFilePath('src', files, 'src')
+
+  allFilesTree['src']['app'][`${req.body['selector']}`] = componentFiles
+
+  //Apply the files tree in the Stackblitz project
+  getFilesPath('src', allFilesTree, 'src')
+
   res.send(project)
 })
-function getFilePath(startDir, fileTree, path){
+
+function getFilesPath(startDir, fileTree, path){
 
   if(typeof fileTree[startDir] !== 'object'){
-     path;
      project['files'][path] = fileTree[startDir]
     return
   }  
   var children = Object.keys(fileTree[startDir])
   children.forEach(child =>  {
-     return getFilePath(child, fileTree[startDir], `${path}/` + child);
+     return getFilesPath(child, fileTree[startDir], `${path}/` + child);
   });
 }
 
@@ -111,7 +134,7 @@ function replaceFlags(fileTree, body){
 
 function replaceComponentFlags(file, body){
   if(body['data']){
-    file = file.replace(/{igDataFile}/g, `import { ${body['data']['name']} } from "./data.ts"`)
+    file = file.replace(/{igDataFile}/g, `import { ${body['data']['name']} } from "./data"`)
     file = file.replace(/{properties}/g, `public data:any;`)
     file = file.replace(/{propertiesDefinition}/g, `this.data = ${body['data']['name']};`)
   } else {
@@ -123,3 +146,7 @@ function replaceComponentFlags(file, body){
     file = file.replace(/{igComponentName}/g, body['name'])
   return file 
 }
+
+app.listen(4205, () => {
+  console.log('Server Started')
+})
