daa5af88e35388c8d5db124d1f053f7629cf0b4e
HristoP96 <HPopov@infragistics.com>
2019-04-05 09:47:07 +0300

    Adding grid features

---
+++ src/app/objects/features.ts
@@ -0,0 +1,9 @@
+export const GridFeatures: any = {
+    Filtering : 'allowFiltering',
+    Paging: 'paging',
+    Hiding: 'columnHiding',
+    Pinning: 'columnPinning',
+    RowEditing: 'rowEditable',
+    RowSelect: 'rowSelectable',
+    Toolbar: 'showToolbar'}
+;
